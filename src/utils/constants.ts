1be9a44194b8a968c68b5df08a7e273633c4b796
Evgeniy Axenov <Bboyyudjin1@yandex.ru>
2025-02-12 22:31:40 +0300

    Начинаем заново

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,6 @@
 export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
-export const productCategoryObject = {
-  'софт-скил': 'soft',
-  'хард-скил': 'hard',
-  'другое': 'other',
-  'дополнительное': 'additional',
-  'кнопка': 'button'
-}
+export const settings = {
+
+};
